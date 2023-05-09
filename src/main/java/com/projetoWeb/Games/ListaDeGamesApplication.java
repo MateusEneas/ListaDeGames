b402a1b6a615d5961d03aaf5841c35ca0fc1bfca
Mateus <mateus.eneas98@gmail.com>
2023-05-09 12:50:23 -0300

    Projeto Criado

---
+++ src/main/java/com/projetoWeb/Games/ListaDeGamesApplication.java
@@ -0,0 +1,13 @@
+package com.projetoWeb.Games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaDeGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaDeGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetoWeb/Games/ListaDeGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoWeb.Games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaDeGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
