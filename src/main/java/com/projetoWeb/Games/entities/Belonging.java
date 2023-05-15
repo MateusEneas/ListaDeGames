31c837199f11689d9e5d07da9a6fba83afd435f7
Mateus <mateus.eneas98@gmail.com>
2023-05-15 11:58:44 -0300

    Implementação do modelo de domínio

---
+++ src/main/java/com/projetoWeb/Games/entities/Belonging.java
@@ -0,0 +1,55 @@
+package com.projetoWeb.Games.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_belonging")
+public class Belonging {
+
+    @EmbeddedId
+    private BelongingPK id = new BelongingPK();
+
+    private Integer posicao;
+
+    public Belonging() {
+    }
+
+    public Belonging(Game game, GameList list , Integer posicao) {
+        id.setGame(game);
+        id.setList(list);
+        this.posicao = posicao;
+    }
+
+    public BelongingPK getId() {
+        return id;
+    }
+
+    public void setId(BelongingPK id) {
+        this.id = id;
+    }
+
+    public Integer getPosicao() {
+        return posicao;
+    }
+
+    public void setPosicao(Integer posicao) {
+        this.posicao = posicao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Belonging belonging = (Belonging) o;
+        return Objects.equals(id, belonging.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
